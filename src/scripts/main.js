2ec3b4d415bffb4dd6b5b5d9d037adbf051d5f86
sirserg <sirserg05@gmail.com>
2025-04-10 20:05:04 +0300

    feat: add game ui

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,86 @@
 const Game = require('../modules/Game.class');
 const game = new Game();
 
-game.getState();
+const startButton = document.querySelector('.button');
+const scoreText = document.querySelector('.game-score');
+const startMessage = document.querySelector('.message-start');
+const winMessage = document.querySelector('.message-win');
+const loseMessage = document.querySelector('.message-lose');
+const tableRows = document.querySelector('.game-field').tBodies[0].rows;
+
+const renderState = () => {
+  const state = game.getState();
+
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (!state[i][j]) {
+        tableRows[i].children[j].textContent = '';
+        tableRows[i].children[j].className = '';
+
+        tableRows[i].children[j].classList.add('field-cell');
+        continue;
+      }
+      tableRows[i].children[j].textContent = state[i][j];
+      tableRows[i].children[j].className = '';
+
+      tableRows[i].children[j].classList.add(
+        'field-cell',
+        `field-cell--${state[i][j]}`,
+      );
+    }
+  }
+};
+
+startButton.addEventListener('click', () => {
+  if (game.getStatus() === 'idle') {
+    game.start();
+
+    startMessage.classList.add('hidden');
+    startButton.classList.remove('start');
+    startButton.classList.add('restart');
+    startButton.textContent = 'Restart';
+  } else {
+    loseMessage.classList.add('hidden');
+    winMessage.classList.add('hidden');
+    game.restart();
+    game.start();
+  }
+
+  renderState();
+});
+
+document.addEventListener('keydown', (e) => {
+  let gameStatus = game.getStatus();
+
+  if (gameStatus === 'lose') {
+    return;
+  }
+
+  switch (e.key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+  }
+
+  gameStatus = game.getStatus();
+
+  if (gameStatus === 'win') {
+    winMessage.classList.remove('hidden');
+  }
+
+  if (gameStatus === 'lose') {
+    loseMessage.classList.remove('hidden');
+  }
+
+  scoreText.textContent = game.getScore();
+  renderState();
+});
