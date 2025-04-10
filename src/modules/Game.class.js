ef79d1f989b4ea1f3d72d30bd38be12b996f74e7
sirserg <sirserg05@gmail.com>
2025-04-10 18:04:40 +0300

    feat: add game logic

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,3 +1,4 @@
+/* eslint-disable function-paren-newline */
 'use strict';
 
 /**
@@ -20,25 +21,234 @@ class Game {
    * If passed, the board will be initialized with the provided
    * initial state.
    */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialState = initialState;
+    this.state = initialState.map((row) => [...row]);
+    this.score = 0;
+    this.status = 'idle';
+    this.dim = 4;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    if (this.status !== 'playing') {
+      return false;
+    }
+
+    const newState = this.state.map((row) => [...row]);
+    const didSwipe = this.swipeRows('left', newState);
+    const didMerge = this.combineRows('left', newState);
+
+    if (didMerge) {
+      this.swipeRows('left', newState);
+    }
+
+    if (didSwipe || didMerge) {
+      this.state = newState;
+      this.generate();
+      this.checkStatus();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  moveRight() {
+    if (this.status !== 'playing') {
+      return false;
+    }
+
+    const newState = this.state.map((row) => [...row]);
+    const didSwipe = this.swipeRows('right', newState);
+    const didMerge = this.combineRows('right', newState);
+
+    if (didMerge) {
+      this.swipeRows('right', newState);
+    }
+
+    if (didSwipe || didMerge) {
+      this.state = newState;
+      this.generate();
+      this.checkStatus();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  moveUp() {
+    if (this.status !== 'playing') {
+      return false;
+    }
+
+    let isChanged = false;
+    const newState = this.state.map((row) => [...row]);
+    const transposed = this.transpose(newState);
+
+    isChanged = this.swipeRows('left', transposed) || isChanged;
+    isChanged = this.combineRows('left', transposed) || isChanged;
+
+    if (isChanged) {
+      this.swipeRows('left', transposed);
+      this.state = this.transpose(transposed);
+      this.generate();
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+  moveDown() {
+    if (this.status !== 'playing') {
+      return false;
+    }
+
+    let isChanged = false;
+    const newState = this.state.map((row) => [...row]);
+    const transposed = this.transpose(newState);
+
+    isChanged = this.swipeRows('right', transposed) || isChanged;
+    isChanged = this.combineRows('right', transposed) || isChanged;
+
+    if (isChanged) {
+      this.swipeRows('right', transposed);
+      this.state = this.transpose(transposed);
+      this.generate();
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  swipeRows(direction = 'left', matrix = this.state) {
+    let isChanged = false;
+
+    for (const row of matrix) {
+      const filteredRow = row.filter((num) => num);
+      const missing = this.dim - filteredRow.length;
+      const zeros = Array(missing).fill(0);
+      const newRow =
+        direction === 'left'
+          ? filteredRow.concat(zeros)
+          : zeros.concat(filteredRow);
+
+      if (row.toString() !== newRow.toString()) {
+        isChanged = true;
+
+        for (let i = 0; i < this.dim; i++) {
+          row[i] = newRow[i];
+        }
+      }
+    }
+
+    return isChanged;
+  }
+
+  combineRows(direction = 'left', matrix = this.state) {
+    let isChanged = false;
+
+    for (const row of matrix) {
+      if (direction === 'left') {
+        for (let i = 0; i < this.dim; i++) {
+          if (row[i] !== 0 && row[i] === row[i + 1]) {
+            if (row[i] === 1024) {
+              this.status = 'win';
+            }
+            isChanged = true;
+            row[i] *= 2;
+            row[i + 1] = 0;
+            this.score += row[i];
+            i++;
+          }
+        }
+      } else {
+        for (let i = this.dim - 1; i >= 0; i--) {
+          if (row[i] !== 0 && row[i] === row[i - 1]) {
+            if (row[i] === 1024) {
+              this.status = 'win';
+            }
+            isChanged = true;
+            row[i] *= 2;
+            row[i - 1] = 0;
+            this.score += row[i];
+            i--;
+          }
+        }
+      }
+    }
+
+    return isChanged;
+  }
+
+  transpose(matrix) {
+    return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
+  }
+
+  generate() {
+    const emptyCells = [];
+
+    for (let i = 0; i < this.dim; i++) {
+      for (let j = 0; j < this.dim; j++) {
+        if (this.state[i][j] === 0) {
+          emptyCells.push([i, j]);
+        }
+      }
+    }
+
+    const [row, col] =
+      emptyCells[Math.floor(Math.random() * emptyCells.length)];
+
+    this.state[row][col] = Math.random() < 0.9 ? 2 : 4;
+
+    return true;
+  }
+
+  checkStatus() {
+    const hasEmptyCell = this.state.some((row) => row.includes(0));
+    const hasMergeableCells = this.state.some((row, rowIndex) => {
+      return row.some((cell, colIndex) => {
+        if (colIndex < this.dim - 1 && cell === row[colIndex + 1]) {
+          return true;
+        }
+
+        if (
+          rowIndex < this.dim - 1 &&
+          cell === this.state[rowIndex + 1][colIndex]
+        ) {
+          return true;
+        }
+
+        return false;
+      });
+    });
+
+    if (!hasEmptyCell && !hasMergeableCells) {
+      this.status = 'lose';
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.score;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.state;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +260,27 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.status;
+  }
 
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.status = 'playing';
+    this.generate();
+    this.generate();
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
-
-  // Add your own methods here
+  restart() {
+    this.score = 0;
+    this.status = 'idle';
+    this.state = this.initialState;
+  }
 }
 
 module.exports = Game;
